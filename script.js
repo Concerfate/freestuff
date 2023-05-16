2ed1f82260845247bfd0ec5831020c0a58d3d5d4
Concerfate <102763434+Concerfate@users.noreply.github.com>
2023-05-16 07:42:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,2 @@
-alert("on dis website you cen get sum free roobex, vbicks, money end sum mincoins")
+alert("on dis website you cen get sum free roobex, vbicks, money end sum mincoins and valorant points and more")
+alert("You can also get free pc's, xbox's, ps4's and more stuf")
